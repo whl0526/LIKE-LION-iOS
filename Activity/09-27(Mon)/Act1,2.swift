cade2423bec33c7adc0fbc45c126205d16d2d41e
이원형 <67450169+whl0526@users.noreply.github.com>
2022-09-27 19:41:04 +0900

    Add files via upload

---
+++ Activity/09-27(Mon)/Act1,2.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+protocol Device {
+    var name: String {get}
+    var color : String {get set}
+    func buildMemoText() -> String
+}
+
+class MyComputer : Device {
+    //여러분의 구현 코드는 여기에...
+    var name : String {
+        get{
+            return "MacBook Pro 2022"
+        }
+    }
+    
+    var color: String{
+        get{
+            return colors
+        }
+        set{
+            colors = newValue + colors
+        }
+    }
+    
+    var colors = "grey"
+    var kind: String = ""
+    var dateOfpurchase: String = ""
+    var price : Int = 0
+    init(kind : String)
+    {
+        self.kind = kind
+    }
+    
+    func displayComputer() -> (String, String, Int){
+        return (kind,dateOfpurchase,price)
+    }
+    
+    func buildMemoText() -> String {
+        return "name : \(name), color: \(color)"
+    }
+}
+    var myMac: MyComputer = MyComputer(kind:"MacBook")
+    myMac.dateOfpurchase = "2022-04-01"
+    myMac.price = 100
+    print("information: \(myMac.displayComputer())")
+    myMac.color = "Space"
+    print(myMac.buildMemoText())
