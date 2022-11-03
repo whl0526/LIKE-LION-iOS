150b685899dad46fa3ed9ae5cfbc187913a8ddc4
이원형 <67450169+whl0526@users.noreply.github.com>
2022-11-03 23:27:19 +0900

    Add files via upload

---
+++ xcode/11-02(Wed-MyScrollView,AnimalPicker)/MyScrollView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct MyScrollView: UIViewRepresentable {
+    
+    var text: String
+    
+    func makeUIView(context: UIViewRepresentableContext<MyScrollView>) -> UIScrollView {
+        let scrollView = UIScrollView()
+        scrollView.delegate = context.coordinator
+        
+        scrollView.refreshControl = UIRefreshControl()
+        scrollView.refreshControl?.addTarget(context.coordinator, action:
+            #selector(Coordinator.handleRefresh),
+                                          for: .valueChanged)
+
+        let myLabel = UILabel(frame:
+                   CGRect(x: 0, y: 0, width: 300, height: 50))
+        myLabel.text = text
+        scrollView.addSubview(myLabel)
+        return scrollView
+    }
+    
+    func updateUIView(_ uiView: UIScrollView,
+                      context: UIViewRepresentableContext<MyScrollView>) {
+        
+    }
+
+    func makeCoordinator() -> Coordinator {
+         Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        var control: MyScrollView
+
+        init(_ control: MyScrollView) {
+            self.control = control
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            print("View is Scrolling")
+        }
+        
+        @objc func handleRefresh(sender: UIRefreshControl) {
+            sender.endRefreshing()
+        }
+    }
+
+}
